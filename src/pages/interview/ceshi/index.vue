77a856e74a4b74a4a18a3e24844dd02cf96d2247
zy <798194200@qq.com>
2021-12-01 16:18:22 +0800

    自定义formite demo

---
+++ src/pages/interview/ceshi/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <el-form-item
+    label="密码"
+    prop="age"
+    :rules="{ validator: validTNumber, trigger: 'blur' }"
+  >
+    <el-input @input="onChange" :value="value"></el-input>
+  </el-form-item>
+</template>
+
+<script>
+export default {
+  name: "ceshihah",
+  model: {
+    prop: "value",
+    event: "change",
+  },
+  props: {
+    value: {
+      type: String,
+    },
+    rules: {
+      type: Object,
+    },
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {
+    onChange(val) {
+      this.$emit("change", val);
+    },
+    validTNumber: function (rule, value, callback) {
+      callback(new Error("请输入正确格式的手机号码"));
+    },
+  },
+};
+</script>
+<style lang='scss' scoped>
+</style>
--- src/pages/interview/index.vue
+++ src/pages/interview/index.vue
@@ -1,20 +1,58 @@
 <template>
-  <div>暂无内容</div>
+  <el-form
+    :model="ruleForm"
+    ref="ruleForm"
+    label-width="100px"
+    class="demo-ruleForm"
+  >
+    <ceshiForm
+      v-model="ruleForm.age"
+      prop="age"
+  />
+  </el-form>
 </template>
-
 <script>
+import ceshiForm from "./ceshi";
 export default {
   name: "interview",
+  components: {
+    ceshiForm,
+  },
+  watch: {
+    ruleForm: {
+      deep: true,
+      handler: (a) => {
+        console.log(666);
+        console.log(a);
+      },
+    },
+  },
   data() {
-    return {}
+    // var validTNumber = (rule, value, callback) => {
+    //   console.log(rule, value, 7777777);
+    //   if (value === "") {
+    //     callback(new Error("请再次输入密码"));
+    //   } else if (value !== this.ruleForm.pass) {
+    //     callback(new Error("两次输入密码不一致!"));
+    //   } else {
+    //     callback();
+    //   }
+    // };
+    return {
+      // validTNumber,
+      ruleForm: {
+        age: "",
+      },
+    };
   },
   methods: {
-    //
-  }
-
-}
+    // validTNumber: function (rule, value, callback) {
+    //   console.log(rule, value, 7777777);
+    //   callback(new Error("请输入正确格式的手机号码"));
+    // },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
